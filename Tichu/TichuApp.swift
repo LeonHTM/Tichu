a28beccf4fe4b3963896bb5e04c1932e2c69be21
LeonHTM <leon.gabriel.strauss@icloud.com>
2026-04-21 12:38:53 +0200

    Initial Commit

---
+++ Tichu/TichuApp.swift
@@ -0,0 +1,17 @@
+//
+//  TichuApp.swift
+//  Tichu
+//
+//  Created by Leon on 21.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct TichuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
