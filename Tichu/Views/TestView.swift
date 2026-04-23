586d1ebfeec46f4d5fa4d04ba50d4e62cd9ce238
LeonHTM <leon.gabriel.strauss@icloud.com>
2026-04-24 00:15:05 +0200

    -Copied ChipsView from a Dude - Improved PlayView - Startet work on StatsView

---
+++ Tichu/Views/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  Tichu
+//
+//  Created by Leon on 23.04.2026.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
