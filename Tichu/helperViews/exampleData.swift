37f055e8e8579245b7e4aa5e7bd902505d5251a8
LeonHTM <leon.gabriel.strauss@icloud.com>
2026-04-24 21:44:32 +0200

    Upgraded StatsView to support true Sorting with all Value being sorted accordingly Moved exmaple Data to own File cleand up statsCOntainerView

---
+++ Tichu/helperViews/exampleData.swift
@@ -0,0 +1,18 @@
+//
+//  exampleData.swift
+//  Tichu
+//
+//  Created by Leon on 24.04.2026.
+//
+
+import SwiftUI
+
+struct exampleData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    exampleData()
+}
