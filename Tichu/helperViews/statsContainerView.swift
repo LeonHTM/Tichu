cb81865d2b79fd6c6b7b9bcfafbd26aa435fd502
LeonHTM <leon.gabriel.strauss@icloud.com>
2026-04-24 19:02:16 +0200

    - Adapted Chipsview to Liquid Glass with the chips mergiing into each other - Created StatsView with support View StatsContainer -Set up Storting Logic

---
+++ Tichu/helperViews/statsContainerView.swift
@@ -0,0 +1,18 @@
+//
+//  statsContainerView.swift
+//  Tichu
+//
+//  Created by Leon on 24.04.2026.
+//
+
+import SwiftUI
+
+struct statsContainerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    statsContainerView()
+}
