55c243a9cdaabad70044f5bc7b2a49bf381971dd
LeonHTM <leon.gabriel.strauss@icloud.com>
2026-04-26 00:51:51 +0200

    Various small improvements

--- Tichu/Logic/profileLogic.swift
+++ Tichu/Logic/profileLogic.swift
@@ -9,8 +9,22 @@ import SwiftUI
 
 struct Profile: Identifiable {
     let id = UUID()
-    let name: String?
-    let imageData: Data?
-    let isFriend: Bool
+    var name: String?
+    var imageData: Data?
+    var isFriend: Bool
+    /*
+     var elo: Int
+     var winnerPercentage: Int
+     var tichuMaster: Double
+     var visionary: Int
+     var addict: Int
+     var teamplayer: Int
+     var announcer: Int
+     var saboteur: Int
+     var gambler: Int
+     var bigGambler: Int
+     var bomber: Int
+     
+     */
 }
 
